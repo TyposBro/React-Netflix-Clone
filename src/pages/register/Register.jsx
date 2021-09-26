3e509e9a8a311b2ae13910db808d28036018f997
@khasanjonovich <khasanjonovich@gmail.com>
2021-09-26 23:26:27 +0900

    register page design is ready

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,22 @@
+import { useRef, useState } from "react";
 import "./Register.scss";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+    console.log(emailRef.current.value);
+  };
+
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
+
   return (
     <div className="register">
       <div className="top">
@@ -20,10 +36,21 @@ const Register = () => {
         <p>
           Ready to watch? Enter your email to create or restart your membership.
         </p>
-        <div className="input">
-          <input type="email" placeholder="email adress" />
-          <button className="registerButton">Get started</button>
-        </div>
+        {!email ? (
+          <div className="input">
+            <input type="email" ref={emailRef} placeholder="email adress" />
+            <button className="registerButton" onClick={handleStart}>
+              Get started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" ref={passwordRef} placeholder="password" />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );

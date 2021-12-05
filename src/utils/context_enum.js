43a9ce9c661429b5d37f27e6cb52707b594cd82a
Azizbek Umidjonov <azizbek@unist.ac.kr>
2021-12-06 08:34:17 +0900

    adding context Enums

---
+++ src/utils/context_enum.js
@@ -0,0 +1,15 @@
+export const contextEnums = {
+  AUTH: {
+    LOGIN: {
+      LOGIN_START: "LOGIN_START",
+      LOGIN_SUCCESS: "LOGIN_SUCCESS",
+      LOGIN_ERROR: "LOGIN_ERROR",
+    },
+    LOGOUT: { LOGOUT: "LOGOUT" },
+    REGISTER: {
+      REGISTER_START: "REGISTER_START",
+      REGISTER_SUCCESS: "REGISTER_SUCCESS",
+      REGISTER_ERROR: "REGISTER_ERROR",
+    },
+  },
+};
